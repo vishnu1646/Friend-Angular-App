a5de01622b8d8b9ff19660c3c4bbf5544758fda3
vishnu1646 <vishnulance98@gmail.com>
2022-11-28 00:46:17 +0530

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddFriendComponent } from './add-friend/add-friend.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddFriendComponent
   ],
   imports: [
     BrowserModule,
