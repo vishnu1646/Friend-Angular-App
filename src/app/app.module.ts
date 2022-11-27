18dab910b49dabd4c1d2e6d41cd7028ff409731e
vishnu1646 <vishnulance98@gmail.com>
2022-11-28 01:01:41 +0530

    viewing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddFriendComponent } from './add-friend/add-friend.component';
+import { ViewFriendComponent } from './view-friend/view-friend.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes=[{
+  path:"",
+  component:AddFriendComponent
+},
+{
+  path:"view",
+  component:ViewFriendComponent
+}]
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddFriendComponent
+    AddFriendComponent,
+    ViewFriendComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
